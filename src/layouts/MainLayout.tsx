d9c9b0467d35ef0a3243f9b3304400784af4dc80
Shira <shira.persky@gmail.com>
2025-11-03 12:24:58 +0200

    added footer

--- src/layouts/MainLayout.tsx
+++ src/layouts/MainLayout.tsx
@@ -8,9 +8,15 @@ export default function MainLayout(){
     return(
         <div className="layout">
             <SideBar/>
+            <div className="layout__content-wrapper">
                 <main className="layout__content">
                     <Outlet/>
                 </main>
+                <footer className="footer">
+                    © 2025 Job Search Management System – MVP Release
+                </footer>
         </div>
+        </div>
+
     );
 }
